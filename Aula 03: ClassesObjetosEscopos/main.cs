14df4672da4cdd4666ceb6d804cbe0250f722605
Elizeu Vito <8630027-ElizeuVito@users.noreply.replit.com>
2024-07-19 17:24:04 +0000

    first commit

--- Aula 03: ClassesObjetosEscopos/main.cs
+++ Aula 03: ClassesObjetosEscopos/main.cs
@@ -2,7 +2,7 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    Console.WriteLine ("Classess, Objetos e Escopos de Visibilidade. \n");
+    Console.WriteLine ("Classes, Objetos e Escopos de Visibilidade. \n");
 
     Produto p1 = new Produto();
     p1.Nome = "Banana";
