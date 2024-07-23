b6a4ff486969bbf18c944275edacb4c9f8ae9e42
Elizeu Vito Santos <94203670+ElizeuV@users.noreply.github.com>
2024-07-23 19:00:12 +0000

    23-07-2024

--- Aula 04: Métodos Construtores e Métodos Comuns/main.cs
+++ Aula 04: Métodos Construtores e Métodos Comuns/main.cs
@@ -1,28 +1,28 @@
-using System;
+// using System;
 
-class MainClass
-{
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Classes, Objetos e Escopos de Visibilidade. \n");
+// class MainClass
+// {
+//     public static void Main(string[] args)
+//     {
+//         Console.WriteLine("Classes, Objetos e Escopos de Visibilidade. \n");
 
-        Produto p1 = new Produto();
-        p1.Nome = "Banana";
-        p1.Preco = 3.9;
-        p1.Comprar(20);
-        p1.Vender(8);
-        Console.WriteLine(p1.ObterTexto());
+//         Produto p1 = new Produto();
+//         p1.Nome = "Banana";
+//         p1.Preco = 3.9;
+//         p1.Comprar(20);
+//         p1.Vender(8);
+//         Console.WriteLine(p1.ObterTexto());
 
-        Produto p2 = new Produto("Laranja", 4.75);
-        p2.Comprar(100);
-        p2.Vender(17);
-        Console.WriteLine(p2.ObterTexto());
+//         Produto p2 = new Produto("Laranja", 4.75);
+//         p2.Comprar(100);
+//         p2.Vender(17);
+//         Console.WriteLine(p2.ObterTexto());
 
-        Produto p3 = new Produto("Abacaxi", 3.25);
-        p3.Comprar(50);
-        p3.Vender(21);
-        Console.WriteLine(p3.ObterTexto());
+//         Produto p3 = new Produto("Abacaxi", 3.25);
+//         p3.Comprar(50);
+//         p3.Vender(21);
+//         Console.WriteLine(p3.ObterTexto());
 
-        //Console.WriteLine($"Estoque = {p3.Estoque}"); //Consigo ver o estoque porem não consigo alterar o estq.
-    }
-}
+//         //Console.WriteLine($"Estoque = {p3.Estoque}"); //Consigo ver o estoque porem não consigo alterar o estq.
+//     }
+// }
---
+++ Aula 05: Estudo de Caso de Cadastro de Produtos/Produto.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Text;
+
+class Produto {
+  private string nome;
+
+
+  public string Nome {
+    get 
+    { //ler
+      return nome;
+    }
+
+    set { //escrever
+      if (value.Length > 1) //O nome do produto deve ter mais de 2 caracteres
+      nome = value;
+      else     
+          throw new Exception("Nome do produto deve ter mais de 2 caracteres");
+    }    
+  }
+
+  public double Preco { get; set; }
+
+  public int Estoque { get; private set; } //pode ser lido, mas so pode ser escrito/alterado dentro da classe produto
+
+  public Produto() {
+     this.Estoque = 0; //this
+  }
+
+  public Produto(string nome, double preco) {
+    this.Nome = nome;
+    this.Preco = preco;
+    this.Estoque = 0;
+  }
+
+  public int Vender(int qtde) {
+    if (this.Estoque - qtde >= 0)
+    this.Estoque -= qtde;
+    return this.Estoque;
+  }
+
+  public int Comprar(int qtde) {
+    this.Estoque += qtde;
+    return this.Estoque;
+  }
+
+  public string ObterTexto() {
+    StringBuilder sb = new StringBuilder();
+    sb.Append($"\nNome: {this.Nome}\n");
+    sb.Append($"Preço: {this.Preco}\n");
+    sb.Append($"Estoque: {this.Estoque}\n");
+    return sb.ToString();
+    
+  }
+
+}
