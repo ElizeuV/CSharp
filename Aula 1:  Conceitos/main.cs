679fbc0e583ad63df098cfefd742aefbe064af35
Elizeu Vito Santos <94203670+ElizeuV@users.noreply.github.com>
2024-07-29 18:43:02 +0000

    29-07-24

---
+++ Aula 1:  Conceitos/main.cs
@@ -0,0 +1,40 @@
+// using System;
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         // Tipos de dados primitivos
+//         int idade = 22;
+//         float temperatura = 36.6f;
+//         double saldo = 1234.56;
+//         char inicial = 'E';
+//         bool isAtivo = true;
+//         decimal preco = 19.99m;
+
+//         // Tipo de dado não primitivo
+//         string nome = "Elizeu";
+
+//         // Exibindo variáveis no console
+//         Console.WriteLine("Idade: " + idade);
+//         Console.WriteLine("Temperatura: " + temperatura);
+//         Console.WriteLine("Saldo: " + saldo);
+//         Console.WriteLine("Inicial: " + inicial);
+//         Console.WriteLine("Ativo: " + isAtivo);
+//         Console.WriteLine("Preço: " + preco);
+//         Console.WriteLine("Nome: " + nome );
+
+//         // Usando interpolação de string (uma forma mais moderna de concatenar strings)
+//         Console.WriteLine($"\nIdade: {idade}");
+//         Console.WriteLine($"Temperatura: {temperatura}");
+//         Console.WriteLine($"Saldo: {saldo}");
+//         Console.WriteLine($"Inicial: {inicial}");
+//         Console.WriteLine($"Ativo: {isAtivo}");
+//         Console.WriteLine($"Preço: {preco}");
+//         Console.WriteLine($"Nome: {nome}");
+
+//         // Espera uma tecla ser pressionada para fechar o console (opcional)
+//         Console.ReadKey();
+//     }
+// }
+
