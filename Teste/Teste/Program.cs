fb7b09c51a4e0fd083548f08451cdbefb5aa5a8f
Elizeu Vito Santos <94203670+ElizeuV@users.noreply.github.com>
2024-08-27 16:12:41 -0300

    Add files via upload

---
+++ Teste/Teste/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//var limite = 1000;
+Console.Write("Por favor, insira um limite: ");
+var limite = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < limite; i++)
+{
+    if (i <= 500)
+    {
+        Console.WriteLine($"Valor do contador atual: {i}");
+
+        if (i == 500)
+        {
+
+            Console.WriteLine($"Valor chegou no limite de {i}");
+        }
+
+        if ((i == 100) || (i == 300))
+        {
+            i += 50; // i = i + 50;
+        }
+    }
+}
+
+
+
